19cfa6d71d5370dc6d0e124cca17ebc9e5549d26
taKana671 <kanae5321@gmail.com>
2025-05-09 01:41:44 +0900

    Changed fyne app.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"GoMaze/maze"
+	"GoMaze/maize"
 	"image"
 
 	"fyne.io/fyne/v2/app"
@@ -12,32 +12,61 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	
-	a := app.New()
-	w := a.NewWindow("Hello")
-	
-	genMaze := func() *image.RGBA {
-		grid := maze.NewWallExtending(31, 31)
-		grid.Create()
-		return grid.Draw()
-	}
 
-	img := genMaze()
+const (
+	FOLLOWING = "Wall Following"
+	DIGGING = "Wall Digging"
+)
+
+
+const (
+	ROWS = 31
+	COLS = 31
+)
+
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("GoMaze")
+
+	img := image.NewRGBA(image.Rect(0, 0,  maize.RECSIZE * ROWS, maize.RECSIZE * COLS))
 	canvasImg := canvas.NewImageFromImage(img)
 	canvasImg.FillMode = canvas.ImageFillOriginal
-	
+
+	genMaze := func(algo string) {
+		var img *image.RGBA
+		
+		switch algo {
+		case FOLLOWING:
+			img = maize.NewFollower(31, 31).Create()
+		case DIGGING:
+			img = maize.NewDigger(31, 31).Create()
+		}
+
+		canvasImg.Image = img
+		canvas.Refresh(canvasImg)
+	}
+
+	sl := widget.NewSelect([]string{
+		FOLLOWING,
+		DIGGING,
+		}, func(s string) {
+			genMaze(s)
+		})
+
+	sl.SetSelected(FOLLOWING)
+
 	tb := widget.NewToolbar(
 		widget.NewToolbarAction(theme.ViewRefreshIcon(), func() {
-			canvasImg.Image = genMaze()
-			canvas.Refresh(canvasImg)
+			genMaze(sl.Selected)
 		}),
 	)
-	
+
 	mainContainer := container.New(
 		layout.NewBorderLayout(
-			nil, tb, nil, nil,
+			sl, tb, nil, nil,
 		),
+		sl,
 		canvasImg,
 		tb,
 	)
